ee6f58a35da670f6410d3fe27cec41a3790ea80d
IronManYG <hussain.yg2010@gmail.com>
2022-03-29 10:55:10 +0300

    Navigation Component

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,10 +1,11 @@
-package com.codinginflow.mvvmtodo
+package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.codinginflow.mvvmtodo.R
 
 
 class TasksFragment : Fragment() {
