79171573ef764966b55d5014467996bdb2ae527d
IronManYG <hussain.yg2010@gmail.com>
2022-03-31 15:22:42 +0300

    ListAdapter with View Binding

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,12 +1,15 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,14 +17,35 @@ class TasksFragment : Fragment() {
 
     private val viewModel: TasksViewModel by viewModels()
 
+    private lateinit var binding: FragmentTasksBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentTasksBinding.inflate(layoutInflater)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -2,6 +2,7 @@ package com.codinginflow.mvvmtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 
 class TasksViewModel
@@ -9,4 +10,6 @@ class TasksViewModel
     constructor(
         private val taskDao: TaskDao
     ) : ViewModel() {
+
+        val tasks = taskDao.getTasks().asLiveData()
 }
